d3b19b3b826a183425c403358b3285f416216460
Clara Meira <claraa.meiraa@hotmail.com>
2024-10-17 14:56:51 -0300

    refazendo conexao

--- demo/src/main/java/com/example/DAO/Conexao.java
+++ demo/src/main/java/com/example/DAO/Conexao.java
@@ -6,28 +6,27 @@ import java.sql.SQLException;
 
 public class Conexao {
     
-    private static final String URL = "jdbc:postgresql://localhost:5432/cantina";
-    private static final String USUARIO = "postgres"; 
-    private static final String SENHA = "postgres"; 
-
-    public static Connection conectar() throws SQLException { 
-        Connection conn = null;
-        
-        try {
-            Class.forName("org.postgresql.Driver");
-            System.out.println("Driver JDBC do PostgreSQL carregado com sucesso!");
-
-            conn = DriverManager.getConnection(URL, USUARIO, SENHA);
-            System.out.println("Conexão estabelecida com sucesso!");
-        } catch (ClassNotFoundException e) {
-            System.err.println("Driver JDBC do PostgreSQL não encontrado. Verifique se a dependência está no classpath.");
-            e.printStackTrace();
+        private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+        private static final String USUARIO = "postgres"; 
+        private static final String SENHA = "postgres"; 
+    
+        public static Connection conectar() {
+            Connection conn = null;
+            
+            try {
+                
+                Class.forName("org.postgresql.Driver");
+                System.out.println("Driver JDBC do PostgreSQL carregado com sucesso!");
+    
+                conn = DriverManager.getConnection(URL, USUARIO, SENHA);
+                System.out.println("Conexão estabelecida com sucesso!");
+            } catch (ClassNotFoundException e) {
+                System.err.println("Driver JDBC do PostgreSQL não encontrado. Verifique se a dependência está no classpath.");
+                e.printStackTrace();
+            } catch (SQLException e) {
+                System.err.println("Erro ao conectar ao PostgreSQL:");
+                e.printStackTrace();
+            }
+            return conn;
         }
-        
-        if (conn == null) {
-            throw new SQLException("Não foi possível estabelecer a conexão com o banco de dados.");
-        }
-
-        return conn;
     }
-}
